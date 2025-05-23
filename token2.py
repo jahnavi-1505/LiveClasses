8df43dbec56d1801106329843fc12d7e860ad793
Jahnavi <singhjahnavi1@gmail.com>
2025-05-23 11:36:50 +0530

    script to get token from graph to fetch the teams meeting url

---
+++ token2.py
@@ -0,0 +1,35 @@
+import os
+from msal import PublicClientApplication
+from dotenv import load_dotenv
+
+load_dotenv()
+TENANT_ID    = os.getenv("TENANT_ID")
+CLIENT_ID    = os.getenv("CLIENT_ID")
+AUTHORITY    = f"https://login.microsoftonline.com/{TENANT_ID}"
+SCOPES = [
+    "https://graph.microsoft.com/User.Read",
+    "https://graph.microsoft.com/Calendars.ReadWrite",
+    "https://graph.microsoft.com/OnlineMeeting.ReadWrite"
+]
+
+
+app = PublicClientApplication(
+    client_id=CLIENT_ID,
+    authority=AUTHORITY
+)
+
+flow = app.initiate_device_flow(scopes=SCOPES)
+if "user_code" not in flow:
+    raise ValueError(f"Failed to start device flow: {flow}")
+
+
+print(flow["message"])  
+
+result = app.acquire_token_by_device_flow(flow)  
+
+if "access_token" in result:
+    print("\n✅ Graph access token:\n")
+    print(result["access_token"])
+else:
+    print("\n❌ Error obtaining token:\n")
+    print(result)
